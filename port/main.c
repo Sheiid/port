0f842661dd2054b248ddef834e06ebbfd9245608
Sheiid <33577644+Sheiid@users.noreply.github.com>
2018-02-15 14:19:04 +0100

    Update main.c

--- port/main.c
+++ port/main.c
@@ -6,82 +6,120 @@
  */
 #include<stdio.h>
 #include<stdlib.h>
-#define N 3
-#define L 10
-#define m 4
 
-void ship_to_port(int,int,int,int *);
-int ship_to_port_R(int **,int,int,int,int *,int,int);
-int check(int**,int,int,int *);
+void ship_to_port(int, int, int, int *);
+void ship_to_port_R(int *, int *,int, int *, int, int, int *, int,int*);
+int check(int *, int, int, int);
 
-int main(){
+int main() {
 
-	int *length;
+	int *length, N = 3, L = 10, m = 4;
 
-	length=(int *)malloc(m*sizeof(int));
-	if(length==NULL){
+	length = (int *) malloc(m * sizeof(int));
+	if (length == NULL) {
 		printf("Allocation Error!");
 		return 1;
 	}
-	ship_to_port(N,L,m,length);
+	length[0]=5;length[1]=8;length[2]=4;length[3]=2;
+	ship_to_port(N, L, m, length);
 
-
-return 0;
+	return 0;
 }
-void ship_to_port(int N,int L,int m,int *length){
+void ship_to_port(int N, int L, int m, int *length) {
 
-	int **piers,i;
-	piers=(int **)malloc(L*sizeof(int*));
-	if(piers== NULL){
+	int *piers,*pierBest;
+	int *mark;
+	int *cnt=m+1;
+	int i,sum=0;
+	int p=1;
+	mark = (int *) calloc(m, sizeof(int));
+	if (mark == NULL) {
+		printf("allocation Error!");
+	}
+	pierBest=(int *)calloc(m,sizeof(int));
+	if (pierBest == NULL) {
+			printf("ALlocation Error!");
+			//exit(EXIT_FAILURE);
+		}
+	piers = (int *) calloc(m, sizeof(int));
+	if (piers == NULL) {
 		printf("ALlocation Error!");
 		//exit(EXIT_FAILURE);
 	}
-	for(i=0;i<N;i++){
-		piers[i]=(int *)malloc(N*sizeof(int));
-		if(piers[i]==NULL){
-			printf("Allocation Error.");
-		}
-	}
-	if(ship_to_port_R(piers,N,L,m,length,0,0)){
-		printf("possible placement for those ships is on piers");
-	}
-	else{
-		printf("!!Solution not found!!");
-	}
-return ;
+
+	ship_to_port_R(piers,pierBest,N, mark, L, m, length, 0,&cnt);
+
+	for (i = 0; i < m && p<=N; i++) {
+					if ((sum += pierBest[i]) <= 10) {
+						printf("pier %d for ships %d \n", p, pierBest[i]);
+					} else {
+						p++;
+						i--;
+						sum = 0;
+					}
+
+				}
+				printf("\n");
+
+	return;
 }
-int ship_to_port_R(int **piers,int N,int L,int m,int *length,int level,int start){
+void ship_to_port_R(int *piers, int *piersBest,int N, int *mark, int L, int m, int *length,int level,int *cnt) {
 
 	int i;
-	if(level==N*L){
-		//check to find minimum N
-		if(check(piers[N][L],m,length,start)){
-			printf("%d , %d",N,length[m]);
-		}
-		printf("\n");
-return 1;
-	}
-	for(i=start;i<L;i++){
-		length[level]=i;
-
-
-		ship_to_port_R(piers,N,L,m,length,level+1,start+l[length]);
-
-	}
-
-}
-int check(int **piers,int m,int *length,int pos){
-	//to find minimum numbers of piers
-	//if there was no free space in a pier in size of m ,go to the next one
-	int i,j;
-	for(i=0;i<m;i++){
-		for(j=0;j<N;j++){
-			if(length[i]>piers[pos]){
-				//go to he next pier!
+	//int sum = 0, p = 1;
+	int nbpiers;
+	if (level == m) {
+		nbpiers=check(piers,N,L,m);
+		if(nbpiers< (*cnt)){
+			*cnt=nbpiers;
+			for(i=0;i<m ;i++){
+				piersBest[i]=piers[i];
 			}
 		}
+
+//		all sol
+//		for (i = 0; i < m && p <= N; i++) {
+//			if ((sum += piers[i]) <= 10) {
+//				printf("%d ships %d \n", p, piers[i]);
+//			} else {
+//				p++;
+//				i--;
+//				sum = 0;
+//			}
+//
+//		}
+//		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < m; i++) {
+		if (mark[i] == 0) {
+			mark[i] = 1;
+			piers[level] = length[i];
+
+			ship_to_port_R(piers,piersBest,N, mark, L, m, length, level + 1,cnt);
+			mark[i] = 0;
+		}
 	}
 
 
+
+	return;
+
 }
+int check (int *piers,int N,int L,int m){
+
+	int i,p=1;
+	int sum=0;
+
+			for (i = 0; i < m && p <= N; i++) {
+				if ((sum += piers[i]) > L) {
+					p++;
+					sum=0;
+					i--;
+				}
+			}
+	return p;
+}
+
 
